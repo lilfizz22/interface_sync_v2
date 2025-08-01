2a587a82f8f987c8da24e632d4cd400386ce6b37
lilfizz22 <lzc20040520lilfizz@163.com>
2025-08-01 13:46:06 +0800

    Update Program.java

--- Program.java
+++ Program.java
@@ -4,7 +4,6 @@ public class Program {
 
     System.out.println("** Adding to SyncEngine...");
 
-    // add contact
     syncEng.add(new Contact("Jeremy", "90111222", "jeremy@door.com"));
     syncEng.add(new Task("Do Homework", 1, 8, 2024));
 
@@ -12,13 +11,10 @@ public class Program {
     task.setDone(true);
     syncEng.add(task);
 
-    // add event
     syncEng.add(new Event("OOPJ Exam", "Room 101", 15, 8, 2024, 9, 0));
 
-    // add note
     syncEng.add(new Note("Shopping List", "Eggs, Milk, Bread"));
 
-    // sync test
     System.out.println("\n** SyncEngine test...");
     syncEng.testSync();
   }
